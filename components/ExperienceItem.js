e33c29195a2e1c6fa0dc515e58fc2f766d8b75f3
Ze Yen <zeyenchai@gmail.com>
2023-04-05 22:23:50 +0800

    Add Edag to working experience

--- components/ExperienceItem.js
+++ components/ExperienceItem.js
@@ -3,43 +3,112 @@ import React from "react";
 const ExperienceItem = () => {
   return (
     <>
-      {/* OmniGeo Solutions */}
+      {/* EDAG Group */}
       <div className="lg:grid grid-cols-5 gap-12 mt-10">
         <div className="col-span-2 mb-10 mb-lg-0">
-          <h2 className="font-normal hidden lg:block">OmniGeo Solutions Sdn Bhd.</h2>
-          <p className="italic mt-3 hidden lg:block">February 2022 - present</p>
+          <h2 className="font-normal hidden lg:block">EDAG Group</h2>
+          <p className="italic mt-3 hidden lg:block">November 2022 - present</p>
         </div>
 
         <div className="col-span-3 mb-10 mb-lg-0">
           <h2 className="pb-2 mb-1">Front-end Software Developer</h2>
 
           <div>
-            <h3 className="text-2xl font-normal lg:hidden">OmniGeo Solutions Sdn Bhd.</h3>
-            <p className="italic mt-3 mb-4 lg:hidden">February 2022 - present</p>
+            <h3 className="text-2xl font-normal lg:hidden">EDAG Group</h3>
+            <p className="italic mt-3 mb-4 lg:hidden">
+              November 2022 - present
+            </p>
           </div>
 
           <div>
             <p className="py-2">
-              Build web applications such as forms and admin system with Next.js and Bootstrap
+              Utilized Angular components, services, modules, and routing to
+              create a modular and maintainable codebase
             </p>
             <p className="py-2">
-              Create functions for uploading, processing and error handling of CSV Files
+              Experienced in resolving complex merge conflicts, successfully
+              resolved 82 commits from 7 different branches in a recent project
+              by utilizing my Git knowledge
             </p>
-            <p className="py-2">Implement user authentication like OAuth with Next Auth</p>
-            <p className="py-2">Create user interface with data retrieved from the server</p>
+            <p className="py-2">
+              Successfully made pull requests incorporating 7 story branches,
+              contributing to seamless code integration and improved
+              collaboration with the development team
+            </p>
+            <p className="py-2">
+              Implemented design changes suggested by clients and team members
+            </p>
+            <p className="py-2">
+              Wrote unit tests using testing tools such as Jest with at least
+              80% of code coverage to ensure the quality of the code and to
+              catch bugs early in the development process
+            </p>
+            <p className="py-2">
+              Collaborated with the back-end team to integrate the front-end
+              with REST APIs and to handle data retrieval and manipulation,
+              resulting in improved data processing speed
+            </p>
+            <p className="py-2">
+              Shared knowledge with colleagues from Malaysia and Germany on
+              front-end topics, including code quality
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* OmniGeo Solutions */}
+      <div className="lg:grid grid-cols-5 gap-12 mt-10">
+        <div className="col-span-2 mb-10 mb-lg-0">
+          <h2 className="font-normal hidden lg:block">
+            OmniGeo Solutions Sdn Bhd.
+          </h2>
+          <p className="italic mt-3 hidden lg:block">
+            September 2021 - October 2022
+          </p>
+        </div>
+
+        <div className="col-span-3 mb-10 mb-lg-0">
+          <h2 className="pb-2 mb-1">Front-end Software Developer</h2>
+
+          <div>
+            <h3 className="text-2xl font-normal lg:hidden">
+              OmniGeo Solutions Sdn Bhd.
+            </h3>
+            <p className="italic mt-3 mb-4 lg:hidden">
+              September 2021 - October 2022
+            </p>
+          </div>
+
+          <div>
+            <p className="py-2">
+              Build web applications such as forms and admin system with Next.js
+              and Bootstrap
+            </p>
+            <p className="py-2">
+              Create functions for uploading, processing and error handling of
+              CSV Files
+            </p>
+            <p className="py-2">
+              Implement user authentication like OAuth with Next Auth
+            </p>
+            <p className="py-2">
+              Create user interface with data retrieved from the server
+            </p>
             <p className="py-2">Submit data to the server with API</p>
             <p className="py-2">
-              Link two sets of data dynamically with Xarrow to create context between data
+              Link two sets of data dynamically with Xarrow to create context
+              between data
             </p>
             <p className="py-2">
-              Using many npm packages to integrate different functionalities to the web app
+              Using many npm packages to integrate different functionalities to
+              the web app
             </p>
           </div>
         </div>
       </div>
 
       {/* B2All Solution */}
-      <div className="lg:grid grid-cols-5 gap-12  mt-10">
+      {/* <div className="lg:grid grid-cols-5 gap-12  mt-10">
         <div className="col-span-2 mb-10 mb-lg-0">
           <h2 className="font-normal hidden lg:block">B2All Solution Sdn. Bhd.</h2>
           <p className="italic mt-3 hidden lg:block">September 2021 - December 2021</p>
@@ -67,13 +136,17 @@ const ExperienceItem = () => {
             <p className="py-2">Migrate more than 10,000 data with Firebase cloud functions</p>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* Jumpstart Commerce Sdn Bhd */}
       <div className="lg:grid grid-cols-5 gap-12  mt-10">
         <div className="col-span-2 mb-10 mb-lg-0">
-          <h2 className="font-normal hidden lg:block">Jumpstart Commerce Sdn. Bhd.</h2>
-          <p className="italic mt-3 hidden lg:block">December 2020 - March 2021</p>
+          <h2 className="font-normal hidden lg:block">
+            Jumpstart Commerce Sdn. Bhd.
+          </h2>
+          <p className="italic mt-3 hidden lg:block">
+            December 2020 - March 2021
+          </p>
         </div>
 
         <div className="col-span-3 mb-10 mb-lg-0">
@@ -83,17 +156,29 @@ const ExperienceItem = () => {
           </h2>
 
           <div>
-            <h3 className="text-2xl font-normal lg:hidden">Jumpstart Commerce Sdn. Bhd.</h3>
-            <p className="italic mt-3 mb-4 lg:hidden">December 2020 - March 2021</p>
+            <h3 className="text-2xl font-normal lg:hidden">
+              Jumpstart Commerce Sdn. Bhd.
+            </h3>
+            <p className="italic mt-3 mb-4 lg:hidden">
+              December 2020 - March 2021
+            </p>
           </div>
 
           <div>
             <p className="py-2">Assist in launching 3 new Shopify stores</p>
             <p className="py-2">Update 6 existing stores in Shopify</p>
-            <p className="py-2">Assist in creating new products and sitemaps for new stores</p>
-            <p className="py-2">Design Shopify store according to client&apos;s needs</p>
-            <p className="py-2">Create store function for clients using jQuery</p>
-            <p className="py-2">Assist in creating CSS templates for the company</p>
+            <p className="py-2">
+              Assist in creating new products and sitemaps for new stores
+            </p>
+            <p className="py-2">
+              Design Shopify store according to client&apos;s needs
+            </p>
+            <p className="py-2">
+              Create store function for clients using jQuery
+            </p>
+            <p className="py-2">
+              Assist in creating CSS templates for the company
+            </p>
           </div>
         </div>
       </div>
@@ -112,16 +197,26 @@ const ExperienceItem = () => {
           </h2>
 
           <div>
-            <h3 className="text-2xl font-normal lg:hidden">Leo Code Sdn. Bhd.</h3>
+            <h3 className="text-2xl font-normal lg:hidden">
+              Leo Code Sdn. Bhd.
+            </h3>
             <p className="italic mt-3 mb-4 lg:hidden">May 2020 - July 2020</p>
           </div>
 
           <div>
-            <p className="py-2">Create webpages with HTML and CSS based on client&apos;s needs</p>
-            <p className="py-2">Check for bugs in the development and production environment</p>
-            <p className="py-2">Translate website content to other languages with Angular i18n</p>
+            <p className="py-2">
+              Create webpages with HTML and CSS based on client&apos;s needs
+            </p>
+            <p className="py-2">
+              Check for bugs in the development and production environment
+            </p>
+            <p className="py-2">
+              Translate website content to other languages with Angular i18n
+            </p>
             <p className="py-2">Provide ideas in creating UI for application</p>
-            <p className="py-2">Assist colleagues in completing tasks and projects</p>
+            <p className="py-2">
+              Assist colleagues in completing tasks and projects
+            </p>
           </div>
         </div>
       </div>
